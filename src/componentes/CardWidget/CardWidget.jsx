fdc5bbfe7a000269972c7ccb3d28b3dd33358547
Candela Ficarra <candelaficarra@gmail.com>
2023-05-31 11:58:24 -0300

    Modifique componentes CardWidget

---
+++ src/componentes/CardWidget/CardWidget.jsx
@@ -0,0 +1,8 @@
+import"./CardWidget.css"
+
+const CardWidget = ()=> {
+        return(
+        <h4 className="carrito">🛒<span className="cantidad">0</span></h4>  
+        )
+}
+export default CardWidget
--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,4 +1,5 @@
 import"./Header.css"
+import CardWidget from "../CardWidget/CardWidget";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -20,7 +21,7 @@ const Header = ()=>{
                     <NavDropdown.Item href="#action/3.2">Hombre</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Niños</NavDropdown.Item>
                   </NavDropdown>
-                  <h4 className="carrito">🛒<span className="cantidad">0</span></h4>
+                  < CardWidget/>
                   </Nav>
               </Navbar.Collapse>
               </Container>
@@ -30,31 +31,3 @@ const Header = ()=>{
 }
 
 export default Header
-// function BasicExample() {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="#home">Tienda Online</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">Home</Nav.Link>
-//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action/3.4">
-//                 Separated link
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default BasicExample;
